8a51e3c136c64141f1c2e182cf885e2766546fdc
d-plaindoux <d.plaindoux@free.fr>
2026-03-06 14:29:57 +0100

    Some examples (refactoring)

--- src/prolog/language/type-checker.pl
+++ src/prolog/language/type-checker.pl
@@ -1,5 +1,5 @@
 /*
- * Cf. https://ncatlab.org/nlab/show/bidirectional+typechecking
+ * Cf. https://ncatlab.org/nlab/show/bidirectional+typechecking [TODO]
  */
 
 :- op(800, xfx, user:(->)).
@@ -11,7 +11,7 @@
  *  ----------------
  *  Gamma |- X <= T
  */
-tc(Gamma, X, T, proof(gamma(X,T))) :-
+type_system(Gamma, X, T, proof(gamma(X,T))) :-
     nonvar(X),
     member(X:T, Gamma),
     !.
@@ -21,9 +21,9 @@ tc(Gamma, X, T, proof(gamma(X,T))) :-
  *  ------------------------------------
  *  Gamma |- X Y => T2
  */
-tc(Gamma, X @ Y, T2, proof(abs,LOG1,LOG2)) :-
-    tc(Gamma, X, T1 -> T2, LOG1),
-    tc(Gamma, Y, T1, LOG2),
+type_system(Gamma, X @ Y, T2, proof(abs,LOG1,LOG2)) :-
+    type_system(Gamma, X, T1 -> T2, LOG1),
+    type_system(Gamma, Y, T1, LOG2),
     !.
 
 /*
@@ -31,22 +31,22 @@ tc(Gamma, X @ Y, T2, proof(abs,LOG1,LOG2)) :-
  *  ----------------------
  *  Gamma |- \X.T <= T1 -> T2
  */
-tc(Gamma, X => T, T1 -> T2, proof(abs, LOG)) :-
+type_system(Gamma, X => T, T1 -> T2, proof(abs, LOG)) :-
     append([X:T1], Gamma, NGamma),
-    tc(NGamma, T, T2, LOG),
+    type_system(NGamma, T, T2, LOG),
     !.
 
 /* Error corner */
 
-tc(Gamma, X, T, proof(error, Gamma, X, T )).
+type_system(Gamma, X, T, proof(error, Gamma, X, T )).
 
 /* Some examples
 
-    :- tc([], x => x,T,L).
+    :- type_system([], x => x,T,L).
     T = (_A->_A),
     L = proof(abs, proof(gamma(x, _A))).
 
-    ?- tc([y:int],(x => x) @ y,T,L).
+    ?- type_system([y:int],(x => x) @ y,T,L).
     T = int,
     L = proof(abs, proof(abs, proof(gamma(x, int))), proof(gamma(y, int))).
 
