c9f5be7a45090251f6b6dc383e31e2aad1115756
d-plaindoux <d.plaindoux@free.fr>
2026-03-06 14:49:02 +0100

    Some examples (refactoring)

--- src/prolog/loan/debt.pl
+++ src/prolog/loan/debt.pl
@@ -11,3 +11,12 @@ debt(family(NetSalary),monthly(Instalment),debtPercent(Percent)) :-
     {Instalment =< MaxDebt / 100 * NetSalary},
     {Percent = 100 * (Instalment / NetSalary)}.
 
+/* Some examples
+
+    :- debt(family(4_500),monthly(1_500),debtPercent(P)).
+    P = 33.333333333333336.
+
+    :- debt(family(4_500),monthly(2_000-X),debtPercent(35)).
+    X = 425.0000000000002.
+
+*/
--- src/prolog/real-estate/incomes.pl
+++ src/prolog/real-estate/incomes.pl
@@ -4,6 +4,8 @@
  * This is a typical database like operation with some constraints
  */
 
+/* Cf. https://www.service-public.gouv.fr/particuliers/vosdroits/F34966/1?idFicheParent=F10871 */
+
 ptzIncomesRow(1, 31_500, 28_500).
 ptzIncomesRow(2, 47_250, 42_750).
 ptzIncomesRow(3, 56_700, 51_300).
@@ -11,7 +13,7 @@ ptzIncomesRow(4, 66_150, 59_850).
 ptzIncomesRow(5, 75_600, 68_400).
 ptzIncomesRow(6, 85_050, 76_950).
 ptzIncomesRow(7, 94_500, 85_500).
-ptzIncomesRow(Members, 103_950, 94_050) :- {Members > 8}.
+ptzIncomesRow(Members, 103_950, 94_050) :- {Members > 7}.
 
 ptzIncomes(family(members(M), incomes(Incomes)), zoneB2) :-
     ptzIncomesRow(M, MaxIncomes, _),
@@ -20,3 +22,10 @@ ptzIncomes(family(members(M), incomes(Incomes)), zoneB2) :-
 ptzIncomes(family(members(Members), incomes(Incomes)), zoneC) :-
     ptzIncomesRow(Members, _, MaxIncomes),
     {Incomes =< MaxIncomes}.
+
+/* Some examples
+
+    :- ptzIncomes(family(members(4), incomes(90_000-Y)), zoneC).
+    {Y>=30150.0}
+
+*/
